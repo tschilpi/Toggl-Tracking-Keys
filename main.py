79a6758103cca393e34a25b99eb498a1cb108580
Marios Vettas <marios.vettas@bluewin.ch>
2022-11-23 14:25:54 +0100

    better approach for starting and ending tracking. Use event handler instead of an infinite while loop.

--- main.py
+++ main.py
@@ -5,31 +5,29 @@ from datetime import datetime
 import time
 import keyboard
 
-#press hotkey to start tracking
+timeentry_id = 0
 
-while True:
-  if keyboard.is_pressed('F1'):  # if key 'q' is pressed
-        now = datetime.utcnow()
-        # RFC 3339 required (add Z)
-        utctime = now.isoformat() + "Z"
-        # negative UNIX timestamp
-        unixtime = int(-time.time())
-        data = requests.post('https://api.track.toggl.com/api/v9/workspaces/4005441/time_entries',
-                             json={"created_with": "python", "pid": 157781596, "workspace_id": 4005441,
-                                   "start": utctime, "user_id": 5446534, "duration": unixtime},
-                             headers={'content-type': 'application/json',
-                                      'Authorization': 'Basic %s' % b64encode(
-                                          b"1310a3434a655a92cb5cd3d26f9b4704:api_token").decode("ascii")})
-        b = json.loads(data.content)
-        timeentry_id = b['id']
-        while True:
-            if keyboard.is_pressed('F2'):
-                data = requests.patch(
-                    f'https://api.track.toggl.com/api/v9/workspaces/4005441/time_entries/{timeentry_id}/stop',
-                    headers={'content-type': 'application/json', 'Authorization': 'Basic %s' % b64encode(
-                        b"1310a3434a655a92cb5cd3d26f9b4704:api_token").decode("ascii")})
-                break
+def start_tracking():
+    now = datetime.utcnow()
+    # RFC 3339 required (add Z)
+    utctime = now.isoformat() + "Z"
+    # negative UNIX timestamp
+    unixtime = int(-time.time())
+    data = requests.post('https://api.track.toggl.com/api/v9/workspaces/4005441/time_entries',
+                         json={"created_with": "python", "pid": 157781596, "workspace_id": 4005441,
+                               "start": utctime, "user_id": 5446534, "duration": unixtime},
+                         headers={'content-type': 'application/json',
+                                  'Authorization': 'Basic %s' % b64encode(
+                                      b"1310a3434a655a92cb5cd3d26f9b4704:api_token").decode("ascii")})
+    b = json.loads(data.content)
+    global timeentry_id
+    timeentry_id = b['id']
+
+def end_tracking():
+    data = requests.patch(f'https://api.track.toggl.com/api/v9/workspaces/4005441/time_entries/{timeentry_id}/stop',
+        headers={'content-type': 'application/json', 'Authorization': 'Basic %s' % b64encode(
+        b"1310a3434a655a92cb5cd3d26f9b4704:api_token").decode("ascii")})
 
 
-
-
+if keyboard.add_hotkey("F1", start_tracking):
+    keyboard.add_hotkey("F2", end_tracking)

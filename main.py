330714ec653076dbebfd29765f9fe4289fa5b0ea
Marios Vettas <marios.vettas@bluewin.ch>
2022-12-19 16:41:30 +0100

    eliminated code. from four functions to two.

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ from datetime import datetime
 import time
 import keyboard
 from threading import Thread
+import chime
 
-timeentry_id = 0
+#toggl time entry globally declared
+#project id's of the hotkeys
+
 CS50 = 157781596
 Mathematics = 165785237
 Real_Analysis = 186010611
 
+#Get user data
 data = requests.get('https://api.track.toggl.com/api/v9/me/projects', headers={'content-type': 'application/json', 'Authorization' : 'Basic %s' %  b64encode(b"1310a3434a655a92cb5cd3d26f9b4704:api_token").decode("ascii")})
 print(data.json())
 
-def start_tracking(project_id):
+#Function to start tracking
+#Input for toggl's API (time_entries endpoint): check out official API's documentation to refers to how to set values (start, duration, etc.)
+def start_tracking(project_id, hotkey):
     now = datetime.utcnow()
     # RFC 3339 required (add Z)
     utctime = now.isoformat() + "Z"
@@ -27,27 +33,26 @@ def start_tracking(project_id):
                                   'Authorization': 'Basic %s' % b64encode(
                                       b"1310a3434a655a92cb5cd3d26f9b4704:api_token").decode("ascii")})
     b = json.loads(data.content)
-    global timeentry_id
     timeentry_id = b['id']
 
-def end_tracking():
+    chime.success()
+    keyboard.remove_hotkey(hotkey)
+    keyboard.add_hotkey(hotkey, end_tracking, args=([timeentry_id, project_id, hotkey]))
+
+#Function to end tracking
+#Refer to readme.txt to learn more about the event loop and how hotkeys are triggered
+def end_tracking(timeentry_id, project_id, hotkey):
     data = requests.patch(f'https://api.track.toggl.com/api/v9/workspaces/4005441/time_entries/{timeentry_id}/stop',
         headers={'content-type': 'application/json', 'Authorization': 'Basic %s' % b64encode(
         b"1310a3434a655a92cb5cd3d26f9b4704:api_token").decode("ascii")})
 
-def started(project_id,hotkey):
-    start_tracking(project_id)
     keyboard.remove_hotkey(hotkey)
-    keyboard.add_hotkey(hotkey, ended, args=([project_id,hotkey]))
+    chime.success()
+    keyboard.add_hotkey(hotkey, start_tracking, args=([project_id, hotkey]))
 
-def ended(project_id,hotkey):
-    end_tracking()
-    keyboard.remove_hotkey(hotkey)
-    keyboard.add_hotkey(hotkey, started, args=([project_id,hotkey]))
+#Hotkey event started separately, in parallel, with threads
 
-#Hotkeys
-
-t1 = Thread(target=keyboard.add_hotkey("F1", started, args=([Mathematics,"F1"])))
-t2 = Thread(target=keyboard.add_hotkey("F2", started, args=([CS50,"F2"])))
-t3 = Thread(target=keyboard.add_hotkey("F3", started, args=([Real_Analysis,"F3"])))
+t1 = Thread(target=keyboard.add_hotkey("F1", start_tracking, args=([Mathematics,"F1"])))
+t2 = Thread(target=keyboard.add_hotkey("F2", start_tracking, args=([CS50,"F2"])))
+t3 = Thread(target=keyboard.add_hotkey("F3", start_tracking, args=([Real_Analysis,"F3"])))
 

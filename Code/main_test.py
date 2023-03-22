fdf7e2af77083cd80e414c24f2a65b26dc3ab32b
Marios Vettas <marios.vettas@bluewin.ch>
2023-03-22 23:30:06 +0100

    Latest version

--- Code/main_test.py
+++ Code/main_test.py
@@ -19,6 +19,7 @@ def fetch_user_data():
         data = requests.get('https://api.track.toggl.com/api/v9/me/projects', headers={'content-type': 'application/json', 'Authorization' : 'Basic %s' %  b64encode(config.byte_string).decode("ascii")})
         try:
             config.enddata = data.json()
+            print(config.enddata)
         except ValueError:
             st.error('Wrong API key entered.', icon="🚨")
 
